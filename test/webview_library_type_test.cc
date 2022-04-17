bbd092be64bf255539a0ecc919d14594c19d2ecf
Steffen André Langnes <steffenl.sw@gmail.com>
2022-04-18 00:31:01 +0900

    Fix lint issues

--- test/webview_library_type_test.cc
+++ test/webview_library_type_test.cc
@@ -2,37 +2,47 @@
 #error WEBVIEW_BUILDING must not be defined
 #endif
 
-#define YES "yes"
-#define NO "no"
-
 #include "webview.h"
-
-#if defined(WEBVIEW_SHARED)
-#define LIBRARY_TYPE "shared"
-#elif defined(WEBVIEW_STATIC)
-#define LIBRARY_TYPE "static"
-#else
-#define LIBRARY_TYPE "header-only"
-#endif
-
-#ifdef WEBVIEW_INCLUDE_IMPLEMENTATION
-#define IMPL_INCLUDED YES
-#else
-#define IMPL_INCLUDED NO
-#endif
-
-#ifdef WEBVIEW_HEADER
-#define IMPL_OPT_OUT YES
-#else
-#define IMPL_OPT_OUT NO
-#endif
-
 #include <iostream>
+
+namespace options {
+
+constexpr auto const yes = "yes"; // NOLINT(unused-const-variable)
+constexpr auto const no = "no";   // NOLINT(unused-const-variable)
+
+constexpr auto const libraryType =
+#if defined(WEBVIEW_SHARED)
+    "shared"
+#elif defined(WEBVIEW_STATIC)
+    "static"
+#else
+    "header-only"
+#endif
+    ;
+
+constexpr auto const implIncluded =
+#ifdef WEBVIEW_INCLUDE_IMPLEMENTATION
+    yes
+#else
+    no
+#endif
+    ;
+
+constexpr auto const implOptOut =
+#ifdef WEBVIEW_HEADER
+    yes
+#else
+    no
+#endif
+    ;
+
+} // namespace options
 
 int main() {
   webview_destroy(webview_create(0, nullptr));
-  std::cout << "Type: " LIBRARY_TYPE << std::endl;
-  std::cout << "Implementation included: " IMPL_INCLUDED << std::endl;
-  std::cout << "Implementation opt-out: " IMPL_OPT_OUT << std::endl;
+  std::cout << "Type: " << options::libraryType << std::endl;
+  std::cout << "Implementation included: " << options::implIncluded
+            << std::endl;
+  std::cout << "Implementation opt-out: " << options::implOptOut << std::endl;
   return 0;
 }
