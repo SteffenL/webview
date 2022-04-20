562cd329d7e3061c82b4b4422c0dcd37ed5ba205
Steffen André Langnes <steffenl.sw@gmail.com>
2022-04-20 10:54:50 +0900

    Turn on compliation warnings
    Disable warning for unused parameters for now.

---
+++ cmake/build_support.cmake
@@ -0,0 +1,16 @@
+if(MSVC)
+    set(COMPILE_OPTIONS
+        # TODO: /WX
+        /W4
+        /wd4100 # unused parameters
+    )
+else()
+    set(COMPILE_OPTIONS
+        # TODO: -Werror
+        -Wall -Wextra -Wconversion -Wsign-conversion -pedantic
+        -Wno-unused-parameter
+    )
+endif()
+set(TARGET_NAME webview_build_support)
+add_library(${TARGET_NAME} INTERFACE)
+target_compile_options(${TARGET_NAME} INTERFACE ${COMPILE_OPTIONS})
