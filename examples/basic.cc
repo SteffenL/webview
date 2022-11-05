3db7bd165ba2107a5898860865eb9f599614d389
Steffen André Langnes <steffenl.sw@gmail.com>
2022-11-05 21:46:27 +0900

    Revert basic.cc example to upstream

--- examples/basic.cc
+++ examples/basic.cc
@@ -1,47 +1,11 @@
 #define WEBVIEW_DEFINE_MAIN
 #include "webview/webview.h"
 
-#include <iostream>
-
-constexpr const auto html = R"|||(
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>webview example</title>
-  </head>
-  <body>
-    <p>Your user agent is as follows.</p>
-    <p><code id="userAgent"></code></p>
-    <p>Try to ping.</p>
-    <button id="ping">Ping</button>
-    <p id="answer"></p>
-    <script>
-      document.getElementById("ping").addEventListener("click", function() {
-        ping({ "message": "Ping!" }, 123).then(function(result) {
-          console.log("Received pong:", JSON.stringify(result));
-          document.getElementById("answer").textContent = result.message;
-        });
-      });
-      document.getElementById("userAgent").textContent = navigator.userAgent;
-    </script>
-  </body>
-</html>
-)|||";
-
 int webview_app_main(int argc, char *argv[]) {
-  using namespace webview::detail;
-  webview::webview w(true, nullptr);
-  w.set_title("Basic example");
+  webview::webview w(false, nullptr);
+  w.set_title("Basic Example");
   w.set_size(480, 320, WEBVIEW_HINT_NONE);
-  w.bind("ping", [](const std::string &s) -> std::string {
-    std::cout << "Received ping: " << s << "\n";
-    auto req_object = json_parse(s, "", 0);
-    auto req_array = json_parse(s, "", 1);
-    auto message = json_parse(req_object, "message", -1);
-    auto number = json_parse(req_array, "", 0);
-    return R"({"message": "Pong )" + number + R"(!"})";
-  });
-  w.set_html(html);
+  w.set_html("Thanks for using webview!");
   w.run();
   return 0;
 }
