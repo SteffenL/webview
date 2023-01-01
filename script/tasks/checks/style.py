3949510984d4262e565db27b19781a5d7f0c5eff
Steffen André Langnes <steffenl.sw@gmail.com>
2023-01-01 20:49:32 +0900

    Improve style check task

--- script/tasks/checks/style.py
+++ script/tasks/checks/style.py
@@ -8,14 +8,15 @@ import subprocess
 
 def check_file(task: Task, file_path: str):
     clang_format_args = ["clang-format", "--Werror", file_path]
-    with subprocess.Popen(clang_format_args, stdout=subprocess.PIPE) as clang_format:
+    with subprocess.Popen(clang_format_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as clang_format:
         git_diff_args = ["git", "diff", "--no-index", "--", file_path, "-"]
-        with subprocess.Popen(git_diff_args, stdin=clang_format.stdout, stdout=subprocess.PIPE) as git_diff:
+        with subprocess.Popen(git_diff_args, stdin=clang_format.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as git_diff:
             clang_format.stdout.close()
             output = git_diff.communicate()[0]
             if output:
-                raise Exception("Code style violation:\n" +
-                                str(output, encoding="utf8"))
+                task.set_result(output.decode("utf8"))
+                raise Exception("Code style violation:\n" + output)
+
 
 def register(task_runner: TaskRunner, workspace: Workspace):
     """Registers tasks for code style checks."""
@@ -29,4 +30,4 @@ def register(task_runner: TaskRunner, workspace: Workspace):
     for _, source in sources:
         full_path = os.path.join(workspace.get_source_dir(), source)
         tasks.add_task(Task(check_file, arg=full_path,
-                    description="Check code style ({})".format(full_path)))
+                            description="Check code style ({})".format(full_path)))
