18318e01e9a6939b11c8aa14979312ae3817c7c0
Steffen André Langnes <steffenl.sw@gmail.com>
2023-01-05 06:17:13 +0900

    wip

--- script/internal/cli.py
+++ script/internal/cli.py
@@ -1,9 +1,15 @@
 from internal.options import Options
 from internal.utility import bool_to_str
 
+from abc import abstractmethod
 import argparse
+from enum import Enum
+import math
+import os
+import platform
 import sys
-from typing import Callable
+from typing import Callable, IO, List, Sequence, Union
+
 
 if sys.version_info >= (3, 8):
     from typing import get_args, get_type_hints
@@ -73,3 +79,159 @@ def parse_options(args, arg_parser_factory: Callable[[], argparse.ArgumentParser
         setattr(options, attr, option_type(
             None if value is None else value_type(value), explicit))
     return options
+
+
+def is_terminal_interactive() -> bool:
+    return sys.stdout.isatty()
+
+
+def terminal_supports_escape() -> bool:
+    # Windows: The Command shell does not support escape codes.
+    #          The Windows Terminal supports escape codes but can we easily and reliably detect this?
+    return is_terminal_interactive() and platform.system() != "Windows"
+
+
+class PrintColor(Enum):
+    RESET = 0
+    FG_RED = 31
+    FG_GREEN = 32
+    FG_BLUE = 36
+    BG_WHITE = 47
+
+
+class PrintCommand:
+    _callback: Callable[[IO], None]
+
+    def __init__(self, callback: Callable[[IO], None]):
+        self._callback = callback
+
+    @abstractmethod
+    def __call__(self, io: IO):
+        self._callback(io)
+
+
+class EscapeCodeCommand(PrintCommand):
+    _escape_chars = "\033["
+
+    def __init__(self, sequence: str):
+        super().__init__(lambda io: io.write(self._escape_chars + sequence))
+
+
+class PrintSequence:
+    _sequence: Sequence[Union[PrintCommand, str]]
+    _enable_commands: bool
+
+    def __init__(self, sequence: Sequence[Union[PrintCommand, str]], with_commands: bool = False):
+        self._sequence = sequence
+        self._enable_commands = with_commands
+
+    def print(self, to: IO = sys.stdout):
+        for s in self._sequence:
+            if isinstance(s, PrintCommand):
+                if self._enable_commands:
+                    s(to)
+            else:
+                to.write(s)
+
+
+class PrintCommandFactory:
+    def color(self, color: PrintColor):
+        return EscapeCodeCommand(f"{color.value}m")
+
+    def save_pos(self):
+        return EscapeCodeCommand("s")
+
+    def restore_pos(self):
+        return EscapeCodeCommand("u")
+
+    def insert_line(self):
+        return EscapeCodeCommand("L")
+
+    def delete_line(self):
+        return EscapeCodeCommand("M")
+
+    def move_y(self, y: int):
+        return EscapeCodeCommand(f"{y}E")
+
+    def flush(self):
+        return PrintCommand(lambda io: io.flush())
+
+    def write(self, s: str):
+        return PrintCommand(lambda io: io.write(s))
+
+
+class PrintSequenceBuilder:
+    _escape_chars = "\033["
+    _result: List[Union[PrintCommand, str]]
+    _factory: PrintCommandFactory
+
+    def __init__(self, factory: PrintCommandFactory = PrintCommandFactory()):
+        self._result = []
+        self._factory = factory
+
+    def to_sequence(self, with_commands: bool = False) -> PrintSequence:
+        return PrintSequence(self._result, with_commands=with_commands)
+
+    def color(self, color: PrintColor):
+        self._result.append(EscapeCodeCommand(f"{color.value}m"))
+
+    def colored(self, start_color: PrintColor, s: str):
+        self.color(start_color)
+        self._result.append(s)
+        self.color(PrintColor.RESET)
+
+    def save_pos(self):
+        self._result.append(self._factory.save_pos())
+
+    def restore_pos(self):
+        self._result.append(self._factory.restore_pos())
+
+    def insert_line(self):
+        self._result.append(self._factory.insert_line())
+
+    def delete_line(self):
+        self._result.append(self._factory.delete_line())
+
+    def move_y(self, y: int):
+        self._result.append(self._factory.move_y(y))
+
+    def raw(self, s: str):
+        self._result.append(s)
+
+    def flush(self):
+        self._result.append(PrintCommand(lambda io: io.flush()))
+
+    def rich(self, *elements: Union[Callable[[IO], None], PrintCommand, str]):
+        for element in elements:
+            if isinstance(element, PrintCommand):
+                self._result.append(element)
+            elif callable(element):
+                self._result.append(PrintCommand(element))
+            elif isinstance(element, str):
+                self._result.append(self._factory.write(element))
+
+    def progress_bar(self, label: str, current: int, total: int, width: int = os.get_terminal_size().columns):
+        before_bar = "".join((
+            label,
+            ": ",
+            (str(math.floor((current / float(total)) * 100)) + "%").ljust(4),
+            "["
+        ))
+        after_bar = "]"
+        other_length = len(before_bar) + len(after_bar)
+        bar_width = width - other_length
+        fill_count = math.floor((current / float(total)) * bar_width)
+        self.insert_line()
+        self.save_pos()
+        self.move_y(9999)
+        self.rich(
+            before_bar,
+            self._factory.color(PrintColor.BG_WHITE),
+            " " * fill_count,
+            self._factory.color(PrintColor.RESET),
+            " " * (bar_width - fill_count),
+            after_bar
+        )
+        self.flush()
+        self.delete_line()
+        self.restore_pos()
--- script/internal/lifecycle.py
+++ script/internal/lifecycle.py
@@ -1,23 +1,16 @@
+from internal.cli import is_terminal_interactive, PrintColor, PrintSequenceBuilder
 from internal.task import TaskRunner, TaskStatus
 
-from abc import abstractmethod
-import math
 import sys
 
 
 class Lifecycle:
-    _color_red = "\033[31m"
-    _color_gray = "\033[34m"
-    _color_green = "\033[32m"
-    _color_reset = "\033[00m"
-    _bgcolor_white = "\033[47m"
-
     _status_width = max(len(s.value) for s in TaskStatus)
     _status_colors = {
-        TaskStatus.CANCELED: _color_red,
-        TaskStatus.FINISHED: _color_green,
-        TaskStatus.FAILED: _color_red,
-        TaskStatus.STARTED: _color_gray
+        TaskStatus.CANCELED: PrintColor.FG_RED,
+        TaskStatus.FINISHED: PrintColor.FG_GREEN,
+        TaskStatus.FAILED: PrintColor.FG_RED,
+        TaskStatus.STARTED: PrintColor.FG_BLUE
     }
 
     _task_runner: TaskRunner
@@ -32,6 +25,7 @@ class Lifecycle:
         if task_count == 0:
             return
 
+        interactive = is_terminal_interactive()
         task_count_digits = len(str(task_count))
         max_progress_length = len("[{}/{}]".format("9".rjust(task_count_digits),
                                                    "9".rjust(task_count_digits)))
@@ -45,33 +39,27 @@ class Lifecycle:
             if status in (TaskStatus.CANCELED, TaskStatus.FINISHED, TaskStatus.FAILED):
                 task_number += 1
 
-            line = ""
+            builder = PrintSequenceBuilder()
+            builder.delete_line()
             if output is not None and len(output) > 0:
-                line += output
-                if not line.endswith("\n"):
-                    line += "\n"
+                builder.raw(output)
+                if not output.endswith("\n"):
+                    builder.raw("\n")
             show_progress = status != TaskStatus.STARTED
             if show_progress:
-                line += "[{}/{}]".format(str(task_number).rjust(task_count_digits),
-                                         str(task_count).rjust(task_count_digits))
+                builder.raw("[{}/{}]".format(str(task_number).rjust(task_count_digits),
+                                             str(task_count).rjust(task_count_digits)))
             else:
-                line += " " * max_progress_length
-            line += " "
+                builder.raw(" " * max_progress_length)
+            builder.raw(" ")
             if status is not None:
-                line += "".join((self._status_colors[status], status.value.upper().ljust(
-                    self._status_width), self._color_reset, " "))
-            line += message
-            sys.stdout.write("\033[M" + line + "\n\033[L\033[s")
-            progress_max_width = 40
-            progress_width = math.floor(
-                (task_number / float(task_count)) * progress_max_width)
-            sys.stdout.write("\033[9999EProgress: {} [{}]".format(
-                (str(math.floor((task_number / float(task_count)) * 100)) + "%").ljust(4),
-                self._bgcolor_white + (" " * progress_width) + self._color_reset +
-                (" " * (progress_max_width - progress_width))
-            ))
-            sys.stdout.flush()
-            sys.stdout.write("\033[M\033[u")
+                builder.colored(
+                    self._status_colors[status], status.value.upper().ljust(self._status_width))
+                builder.raw(" ")
+            builder.raw(message)
+            builder.raw("\n")
+            builder.progress_bar("Progress", task_number, task_count)
+            builder.to_sequence(with_commands=interactive).print()
 
             if exception is not None:
                 raise exception
