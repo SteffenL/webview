7753e613f5bae5d19a75ae80a6ab2b2f4ee84cb4
Steffen André Langnes <steffenl.sw@gmail.com>
2023-01-10 11:56:48 +0900

    Fix Unix script file search

--- script/build.sh
+++ script/build.sh
@@ -12,22 +12,19 @@ else
 	CXXFLAGS="-DWEBVIEW_GTK -std=c++11 $FLAGS $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0)"
 fi
 
+TEST_CXXFLAGS="$CXXFLAGS -I$DIR/test/include"
+
 if command -v clang-format >/dev/null 2>&1 ; then
 	echo "Formatting..."
-	clang-format -i \
-		"$DIR/webview.h" \
-		"$DIR/webview_test.cc" \
-		"$DIR/examples/"*.c \
-		"$DIR/examples/"*.cc
+	clang-format -i $(find "$DIR" -type f -regextype egrep -regex '.*\.(c|cc|h|hpp)')
 else
 	echo "SKIP: Formatting (clang-format not installed)"
 fi
 
 if command -v clang-tidy >/dev/null 2>&1 ; then
 	echo "Linting..."
-	clang-tidy "$DIR/examples/basic.cc" -- $CXXFLAGS
-	clang-tidy "$DIR/examples/bind.cc" -- $CXXFLAGS
-	clang-tidy "$DIR/webview_test.cc" -- $CXXFLAGS
+	clang-tidy $(find "$DIR/examples" -type f -name "*.c") -- $CFLAGS
+	clang-tidy $(find "$DIR/test" -type f -name "*.cc") -- $TEST_CXXFLAGS
 else
 	echo "SKIP: Linting (clang-tidy not installed)"
 fi
@@ -39,7 +36,7 @@ c++ examples/basic.cc $CXXFLAGS -o build/examples/cc/basic
 c++ examples/bind.cc $CXXFLAGS -o build/examples/cc/bind
 
 echo "Building C examples"
-c++ -c $CXXFLAGS webview.cc -o build/webview.o
+c++ -c $CXXFLAGS -DWEBVIEW_STATIC webview.cc -o build/webview.o
 cc -c examples/basic.c $CFLAGS -o build/examples/c/basic.o
 cc -c examples/bind.c $CFLAGS -o build/examples/c/bind.o
 c++ build/examples/c/basic.o build/webview.o $CXXFLAGS -o build/examples/c/basic
@@ -51,9 +48,7 @@ go build -o build/examples/go/bind examples/bind.go
 
 echo "Building test app"
 c++ $(find "$DIR/test/src" -type f -name "*.cc") \
-	"-I$DIR" \
-	"-I$DIR/test/include" \
-	$CXXFLAGS -o webview_test
+	$TEST_CXXFLAGS -o webview_test
 
 echo "Running tests"
 ./webview_test
