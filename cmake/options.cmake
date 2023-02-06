b8b7e2f673e56132f7c1b3542655528a4d51ba03
Steffen André Langnes <steffenl.sw@gmail.com>
2023-02-06 16:12:21 +0900

    wip

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,5 @@
+option(WEBVIEW_USE_STATIC_RUNTIME "Link standard runtime libraries statically" OFF)
+
 if(WIN32)
     set(WEBVIEW_MSWEBVIEW2_VERSION 1.0.1185.39 CACHE STRING "WebView2 SDK version")
     option(WEBVIEW_MSWEBVIEW2_BUILTIN_IMPL "Enable built-in WebView2Loader implementation" ON)
