ea51d2503ccb143a44237a9bdad85f2a4baf4f13
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-05 22:16:00 +0900

    Add Cocoa example

---
+++ examples/cocoa/Cocoa Example/Cocoa Example/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Cocoa Example
+//
+//  Created by Steffen on 2023/08/05.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
