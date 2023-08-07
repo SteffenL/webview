743613812200a0aa4a66115d158d6a04e7e42fb5
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-08 03:15:52 +0900

    wip

--- examples/cocoa/Cocoa Example/ViewController.mm
+++ examples/cocoa/Cocoa Example/ViewController.mm
@@ -17,11 +17,9 @@ std::unique_ptr<webview::webview> m_webview;
 
     // Do any additional setup after loading the view.
 
-    //auto *container{(__bridge void *) _webContainer};
-    NSWindow *window = _webContainer.window; //[[NSApplication sharedApplication] mainWindow];
-    m_webview = std::unique_ptr<webview::webview>(new webview::webview{false, window});
+    m_webview = std::unique_ptr<webview::webview>(new webview::webview{false, _webContainer.window});
     NSView *widget = (NSView *)m_webview->widget();
-    widget.bounds = _webContainer.bounds;
+    widget.frame = _webContainer.frame;
     widget.autoresizesSubviews = YES;
     widget.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
     [_webContainer addSubview:(NSView *)m_webview->widget()];
