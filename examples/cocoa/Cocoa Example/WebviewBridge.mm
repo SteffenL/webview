2b78cec596f137c10c4297a754216f8e861c16ea
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-08 18:31:39 +0900

    fix block retain

--- examples/cocoa/Cocoa Example/WebviewBridge.mm
+++ examples/cocoa/Cocoa Example/WebviewBridge.mm
@@ -33,14 +33,17 @@ std::unique_ptr<webview::webview> _webview;
 
 - (void)bindWithName:(NSString *)name block:(NSString *(^)(NSString * req))block {
     const std::string name_{name.UTF8String, [name lengthOfBytesUsingEncoding:NSUTF8StringEncoding]};
+    id retainedBlock{(id)_Block_copy(block)}; //objc_retainBlock()
     _webview->bind(name_, [=](const std::string& req_) -> std::string {
-        if (block) {
-            NSString *req{[NSString stringWithUTF8String:req_.c_str()]};
-            NSString *res{block(req)};
-            std::string res_{res.UTF8String, [res lengthOfBytesUsingEncoding:NSUTF8StringEncoding]};
-            return res_;
+        if (!retainedBlock) {
+            return {};
         }
-        return {};
+        NSString *req{[NSString stringWithUTF8String:req_.c_str()]};
+        auto typedRetainedBlock = (NSString *(^)(NSString * req))retainedBlock;
+        NSString *res{typedRetainedBlock(req)};
+        //[retainedBlock release]; // causes crash the 2nd time around
+        std::string res_{res.UTF8String, [res lengthOfBytesUsingEncoding:NSUTF8StringEncoding]};
+        return res_;
     });
 }
 
