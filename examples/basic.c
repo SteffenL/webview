30e546c2563205a73165fb3f21c8572cd969b40b
Steffen André Langnes <steffenl.sw@gmail.com>
2023-11-29 15:50:23 +0900

    wip

--- examples/basic.c
+++ examples/basic.c
@@ -16,9 +16,9 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
 int main() {
 #endif
   webview_t w = webview_create(0, NULL);
-  webview_set_title(w, "Basic Example");
+  webview_set_title(w, "こんにちは, 你好, 안녕하세요, привет");
   webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-  webview_set_html(w, "Thanks for using webview!");
+  webview_set_html(w, "こんにちは, 你好, 안녕하세요, привет");
   webview_run(w);
   webview_destroy(w);
   return 0;
