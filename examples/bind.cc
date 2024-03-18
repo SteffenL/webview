fc5cefab9052b9323d7ebcd2a38bea4323129d74
Steffen André Langnes <steffenl.sw@gmail.com>
2024-03-18 14:32:44 +0900

    Revert "Rework error handling and reporting (#1099)"
    This reverts commit 141c23e9f10307d66247cbc23aa2d3fae0e9eab6.

--- examples/bind.cc
+++ examples/bind.cc
@@ -1,7 +1,6 @@
 #include "webview.h"
 
 #include <chrono>
-#include <iostream>
 #include <string>
 #include <thread>
 
@@ -44,41 +43,36 @@ int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
 #else
 int main() {
 #endif
-  try {
-    long count = 0;
+  long count = 0;
 
-    webview::webview w(true, nullptr);
-    w.set_title("Bind Example");
-    w.set_size(480, 320, WEBVIEW_HINT_NONE);
+  webview::webview w(true, nullptr);
+  w.set_title("Bind Example");
+  w.set_size(480, 320, WEBVIEW_HINT_NONE);
 
-    // A binding that counts up or down and immediately returns the new value.
-    w.bind("count", [&](const std::string &req) -> std::string {
-      // Imagine that req is properly parsed or use your own JSON parser.
-      auto direction = std::stol(req.substr(1, req.size() - 1));
-      return std::to_string(count += direction);
-    });
+  // A binding that counts up or down and immediately returns the new value.
+  w.bind("count", [&](const std::string &req) -> std::string {
+    // Imagine that req is properly parsed or use your own JSON parser.
+    auto direction = std::stol(req.substr(1, req.size() - 1));
+    return std::to_string(count += direction);
+  });
 
-    // A binding that creates a new thread and returns the result at a later time.
-    w.bind(
-        "compute",
-        [&](const std::string &id, const std::string &req, void * /*arg*/) {
-          // Create a thread and forget about it for the sake of simplicity.
-          std::thread([&, id, req] {
-            // Simulate load.
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            // Imagine that req is properly parsed or use your own JSON parser.
-            const auto *result = "42";
-            w.resolve(id, 0, result);
-          }).detach();
-        },
-        nullptr);
+  // A binding that creates a new thread and returns the result at a later time.
+  w.bind(
+      "compute",
+      [&](const std::string &id, const std::string &req, void * /*arg*/) {
+        // Create a thread and forget about it for the sake of simplicity.
+        std::thread([&, id, req] {
+          // Simulate load.
+          std::this_thread::sleep_for(std::chrono::seconds(1));
+          // Imagine that req is properly parsed or use your own JSON parser.
+          const auto *result = "42";
+          w.resolve(id, 0, result);
+        }).detach();
+      },
+      nullptr);
 
-    w.set_html(html);
-    w.run();
-  } catch (const webview::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
+  w.set_html(html);
+  w.run();
 
   return 0;
 }
