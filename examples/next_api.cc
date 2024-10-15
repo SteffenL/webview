118c47ae2bd97721f91ca266e013d0040d2bddb5
Steffen André Langnes <steffenl.sw@gmail.com>
2024-10-16 02:32:06 +0900

    wip

--- examples/next_api.cc
+++ examples/next_api.cc
@@ -8,22 +8,22 @@
 
 class sub_window {
 public:
-  sub_window() : m_window{webview::window_options{}.set_title("Sub Window")} {
-    m_window.get_widget().set_html(make_html(sub_window_html));
-    m_window.set_visible(true);
+  sub_window() : m_inner{webview::window_options{}.set_title("Sub Window")} {
+    m_inner.get_widget().set_html(make_html(sub_window_html));
+    m_inner.set_visible(true);
   }
 
 private:
-  webview::window m_window;
+  webview::window m_inner;
 };
 
 class main_window {
 public:
-  main_window() : m_window{webview::window_options{}.set_title("Main Window")} {
-    m_window.events().close_requested.bind(
+  main_window() : m_inner{webview::window_options{}.set_title("Main Window")} {
+    m_inner.events().close_requested.bind(
         [] { std::cout << "Close requested\n"; });
-    m_window.get_widget().set_html(make_html(main_window_html));
-    m_window.set_visible(true);
+    m_inner.get_widget().set_html(make_html(main_window_html));
+    m_inner.set_visible(true);
   }
 
 private:
@@ -33,12 +33,12 @@ private:
     m_sub_window_map.emplace(ptr.get(), it);
   }
 
-  //void cmd_request_close() { m_window.destroy(); }
-  void cmd_close() { m_window.close(); }
+  //void cmd_request_close() { m_inner.destroy(); }
+  void cmd_close() { m_inner.close(); }
   void cmd_fullscreen() {}
   void cmd_unfullscreen() {}
 
-  webview::window m_window;
+  webview::window m_inner;
   std::list<std::shared_ptr<sub_window>> m_sub_windows;
   std::unordered_map<sub_window *, typename decltype(m_sub_windows)::iterator>
       m_sub_window_map;
