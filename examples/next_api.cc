a5eff5b145302260b931ec49c8794c0239dec9bd
Steffen André Langnes <steffenl.sw@gmail.com>
2024-10-16 17:53:50 +0900

    wip

--- examples/next_api.cc
+++ examples/next_api.cc
@@ -8,18 +8,18 @@
 
 class sub_window {
 public:
-  sub_window() : m_inner{webview::window_options{}.set_title("Sub Window")} {
+  sub_window() {
     m_inner.get_widget().set_html(make_html(sub_window_html));
     m_inner.set_visible(true);
   }
 
 private:
-  webview::window m_inner;
+  webview::window m_inner{webview::window_options{}.set_title("Sub Window")};
 };
 
 class main_window {
 public:
-  main_window() : m_inner{webview::window_options{}.set_title("Main Window")} {
+  main_window() {
     m_inner.events().close_requested.bind([=] {
       std::cout << "Close requested\n";
       m_inner.destroy();
@@ -40,7 +40,7 @@ private:
   void cmd_fullscreen() {}
   void cmd_unfullscreen() {}
 
-  webview::window m_inner{};
+  webview::window m_inner{webview::window_options{}.set_title("Main Window")};
   std::list<std::shared_ptr<sub_window>> m_sub_windows;
   std::unordered_map<sub_window *, typename decltype(m_sub_windows)::iterator>
       m_sub_window_map;
