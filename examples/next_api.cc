f50158a3041e2861fd695080b5eff3687821027b
Steffen André Langnes <steffenl.sw@gmail.com>
2024-10-28 19:51:49 +0900

    wip

--- examples/next_api.cc
+++ examples/next_api.cc
@@ -1,67 +1,12 @@
 #include "resources/next_api/html.hh"
 #include "webview/experimental/task_executor.hh"
 #include "webview/experimental/webview.hh"
+#include "webview/experimental/window_manager.hh"
 
 #include <atomic>
 #include <chrono>
-#include <list>
 #include <memory>
 #include <thread>
-#include <unordered_map>
-
-class sub_window {
-public:
-  sub_window() {
-    w.browser()->set_html(make_html(sub_window_html));
-    w.set_visible(true);
-  }
-
-private:
-  webview::window w{
-      webview::window_options{}.set_size({480, 320}).set_title("Sub Window")};
-};
-
-class main_window {
-public:
-  main_window() {
-    w.events().destroy.bind([&](webview::iwindow * /*sender*/) {
-      webview::current_application().terminate();
-      return true;
-    });
-    add_bindings();
-    w.browser()->set_html(make_html(main_window_html));
-    w.set_visible(true);
-  }
-
-private:
-  void add_bindings() {
-    auto bridge{w.browser()->bridge()};
-    bridge->bind("cmdNewWindow", [=] {
-      std::shared_ptr<sub_window> ptr{new sub_window{}};
-      auto it{m_sub_windows.insert(m_sub_windows.end(), ptr)};
-      m_sub_window_map.emplace(ptr.get(), it);
-    });
-    bridge->bind("cmdCloseWindow", [=] { w.close(); });
-    bridge->bind("cmdFullscreen", [=] { w.set_fullscreen(true); });
-    bridge->bind("cmdUnfullscreen", [=] { w.set_fullscreen(false); });
-    bridge->bind("cmdTasks", [=](webview::binding_arg &arg) {
-      m_tasks.put(
-          [](webview::binding_promise promise) {
-            static std::atomic_int i{};
-            std::this_thread::sleep_for(std::chrono::seconds{1});
-            promise.resolve(std::to_string(++i));
-          },
-          arg.get_promise());
-    });
-  }
-
-  webview::window w{
-      webview::window_options{}.set_size({480, 320}).set_title("Main Window")};
-  std::list<std::shared_ptr<sub_window>> m_sub_windows;
-  std::unordered_map<sub_window *, typename decltype(m_sub_windows)::iterator>
-      m_sub_window_map;
-  webview::task_executor m_tasks;
-};
 
 #ifdef _WIN32
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -69,7 +14,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 int main() {
 #endif
   webview::application app;
-  main_window main_window;
+  webview::window_manager wm;
+  webview::task_executor tasks;
+
+  auto main{wm.new_window(
+      webview::window_options{}.set_size({480, 320}).set_title("Main Window"))};
+  auto main_bridge{main->browser()->bridge()};
+  main_bridge->bind("cmdNewWindow", [&wm] {
+    auto sub{wm.new_window(webview::window_options{}
+                               .set_size({480, 320})
+                               .set_title("Sub Window"))};
+    sub->browser()->bridge()->bind("cmdCloseWindow", [=] { sub->close(); });
+    sub->browser()->set_html(make_html(sub_window_html));
+    sub->set_visible(true);
+  });
+  main_bridge->bind("cmdCloseWindow", [=] { main->close(); });
+  main_bridge->bind("cmdFullscreen", [=] { main->set_fullscreen(true); });
+  main_bridge->bind("cmdUnfullscreen", [=] { main->set_fullscreen(false); });
+  main_bridge->bind("cmdTasks", [&tasks](webview::binding_arg &arg) {
+    tasks.put(
+        [](webview::binding_promise promise) {
+          static std::atomic_int i{};
+          std::this_thread::sleep_for(std::chrono::seconds{1});
+          promise.resolve(std::to_string(++i));
+        },
+        arg.get_promise());
+  });
+  main->browser()->set_html(make_html(main_window_html));
+  main->set_visible(true);
+
   app.run();
   return 0;
 }
