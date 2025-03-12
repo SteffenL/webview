cd751c8f8e0a0bdf3035107a6928187e5eca46bc
Steffen André Langnes <steffenl.sw@gmail.com>
2025-03-12 15:06:41 +0900

    wip

--- cmake/webview.cmake
+++ cmake/webview.cmake
@@ -1,6 +1,6 @@
 macro(webview_options)
     if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-        set(WEBVIEW_MSWEBVIEW2_VERSION "1.0.1150.38" CACHE STRING "MS WebView2 version")
+        set(WEBVIEW_MSWEBVIEW2_VERSION "1.0.3124.44" CACHE STRING "MS WebView2 version")
         option(WEBVIEW_USE_BUILTIN_MSWEBVIEW2 "Use built-in MS WebView2" ON)
     endif()
 endmacro()
