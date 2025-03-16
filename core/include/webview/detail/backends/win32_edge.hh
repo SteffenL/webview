b225ad09a6376203b86e205c037befb359b37670
Steffen André Langnes <steffenl.sw@gmail.com>
2025-03-16 20:11:11 +0900

    Avoid some excess dispatching

--- core/include/webview/detail/backends/win32_edge.hh
+++ core/include/webview/detail/backends/win32_edge.hh
@@ -681,10 +681,8 @@ protected:
     auto res =
         m_webview->AddScriptToExecuteOnDocumentCreated(wjs.c_str(), &handler);
     if (SUCCEEDED(res)) {
-      // Sadly we need to pump the even loop in order to get the script ID.
-      while (!done) {
-        deplete_run_loop_event_queue();
-      }
+      // Sadly we need to pump the event loop in order to get the script ID.
+      run_event_loop_while([&] { return !done; });
     }
     // TODO: There's a non-zero chance that we didn't get the script ID.
     //       We need to convey the error somehow.
@@ -861,7 +859,12 @@ private:
   void deplete_run_loop_event_queue() {
     bool done{};
     dispatch([&] { done = true; });
-    while (!done) {
+    run_event_loop_while([&] { return !done; });
+  }
+
+  // Blocks while depleting the run loop of events.
+  template <typename Callable> void run_event_loop_while(Callable &&fn) {
+    while (fn()) {
       MSG msg;
       if (GetMessageW(&msg, nullptr, 0, 0) > 0) {
         TranslateMessage(&msg);
