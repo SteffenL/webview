38b805a4423d50c4f3e528b612eefe297cd1be97
Steffen André Langnes <steffenl.sw@gmail.com>
2025-03-20 23:06:52 +0900

    Use G_APPLICATION_FLAGS_NONE despite deprecation

--- examples/gtk3/main.cc
+++ examples/gtk3/main.cc
@@ -108,7 +108,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 int main(int argc, char **argv) {
   app_context_t app_context;
   auto *app{
-      gtk_application_new("dev.webview.example", G_APPLICATION_DEFAULT_FLAGS)};
+      gtk_application_new("dev.webview.example", G_APPLICATION_FLAGS_NONE)};
   g_signal_connect(app, "activate", G_CALLBACK(activate), &app_context);
   auto status{g_application_run(G_APPLICATION(app), argc, argv)};
   g_object_unref(app);
