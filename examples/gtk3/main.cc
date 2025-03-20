3eb3d965a5bccee71a75ef97b8ba063dd75f8046
Steffen André Langnes <steffenl.sw@gmail.com>
2025-03-20 23:25:44 +0900

    Avoid G_APPLICATION_FLAGS_NONE/G_APPLICATION_DEFAULT_FLAGS warning

--- examples/gtk3/main.cc
+++ examples/gtk3/main.cc
@@ -107,8 +107,10 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
 int main(int argc, char **argv) {
   app_context_t app_context;
-  auto *app{
-      gtk_application_new("dev.webview.example", G_APPLICATION_FLAGS_NONE)};
+  auto *app{gtk_application_new(
+      "dev.webview.example",
+      static_cast<GApplicationFlags>(
+          0) /*G_APPLICATION_FLAGS_NONE or G_APPLICATION_DEFAULT_FLAGS*/)};
   g_signal_connect(app, "activate", G_CALLBACK(activate), &app_context);
   auto status{g_application_run(G_APPLICATION(app), argc, argv)};
   g_object_unref(app);
